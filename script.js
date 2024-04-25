ca72270f2d8eaa500737e8586871dc5f55f19985
Ankit Singh <83347882+AnkitSingh1203@users.noreply.github.com>
2024-04-25 23:27:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// let addToDoButton = document.getElementById('addToDo');
-// let toDoContainer = document.getElementById('toDoContainer');
-// let inputField = document.getElementById('inputField');
-
-// addToDoButton.addEventListener('click', function(){
-//     var paragraph = document.createElement('p');
-//     paragraph.classList.add('paragraph-styling');
-//     paragraph.innerText = inputField.value;
-
-//     toDoContainer.appendChild(paragraph);
-
-//     inputField.value = "";
-
-//     paragraph.addEventListener('click',function(){
-//         paragraph.style.textDecoration = 'line-through';
-//     })
-
-//     paragraph.addEventListener('dblclick', function(){
-//         toDoContainer.removeChild(paragraph);
-//     })
-// })
-
-
 const taskInput = document.querySelector(".task-input input"),
 filters = document.querySelectorAll(".filters span"),
 clearAll = document.querySelector(".clear-btn"),
